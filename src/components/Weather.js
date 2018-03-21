7751f867911d2fd2a25306d196d2c8a8ea6ba13e
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-21 16:44:23 +0700

    abcd

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export class Weather extends Component {
     componentDidMount() {
         const url = 'http://api.openweathermap.org/data/2.5/weather?appid=01cc37655736835b0b75f2b395737694&units=metric&q=';
+        axios.get(url + 'SaiGon')
+        .then(x => console.log(x.data.main.temp));
     }
     render() {
         return (
